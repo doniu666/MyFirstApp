3991e78e4671b1eba8b5e68f0973b12defad6196
dekas <doniu666@gmai.com>
2023-03-13 15:19:54 +0100

    pierwsza wersja

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("moja pierwsza apka po latach");
+        Scanner sc=new Scanner(System.in);
+        String list= sc.nextLine();
+        System.out.println("zmienna list ma wartość:  "+list);
+    }
+}
